119e69ff6ac5e6a4edfb79578d81532f77bdd00c
Ganesh <ganeshkaradagi@gmail.com>
2026-08-28 22:26:36 +0530

    Add Terraform outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,34 @@
 output "cluster_name" {
-  value = module.eks.cluster_name
+  description = "EKS Cluster Name"
+  value       = module.eks.cluster_name
 }
 
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
 }
 
 output "bastion_public_ip" {
-  value = module.bastion_host.public_ip
+  description = "Public IP of Bastion Host"
+  value       = module.bastion_host.public_ip
 }
 
 output "rds_endpoint" {
-  value = aws_db_instance.postgres.endpoint
+  description = "RDS PostgreSQL Endpoint"
+  value       = aws_db_instance.postgres.endpoint
 }
 
 output "rds_database_name" {
-  value = aws_db_instance.postgres.db_name
+  description = "RDS Database Name"
+  value       = aws_db_instance.postgres.db_name
+}
+
+output "public_subnets" {
+  description = "Public Subnet IDs"
+  value       = module.vpc.public_subnets
+}
+
+output "private_subnets" {
+  description = "Private Subnet IDs"
+  value       = module.vpc.private_subnets
 }
